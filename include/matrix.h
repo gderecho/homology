95037cc1f7b7213c8364a110b561f933d83c96e6
gderecho <gderecho@users.noreply.github.com>
2019-09-09 00:45:02 -0500

    Add adition to matrix

--- include/matrix.h
+++ include/matrix.h
@@ -4,6 +4,7 @@
 #include <ostream>
 #include <vector>
 #include <initializer_list>
+#include <algorithm>
 
 namespace homology {
 
@@ -14,9 +15,12 @@ private:
     using ilist = std::initializer_list<std::vector<R>>;
 
     vec2d data_;
+    size_t rowdim_;
+    size_t coldim_;
 
 public:
-    constexpr Matrix(ilist init_list) noexcept;
+    Matrix(ilist);
+    Matrix(vec2d);
 
     Matrix(const Matrix &) = default;
     Matrix(Matrix &&) = default;
@@ -24,11 +28,57 @@ public:
     Matrix &operator=(const Matrix &) = default;
     Matrix &operator=(Matrix &&) = default;
 
+    size_t rowdim() const {return rowdim_;}
+    size_t coldim() const {return coldim_;}
+
+    /*
+     * friend Matrix operator+
+     * 
+     * Assumes left and right have the same dimensions.
+     *
+     */
+    friend Matrix operator+(const Matrix &left, const Matrix &right)
+    {
+        size_t rowdim = left.rowdim();
+        size_t coldim = left.coldim();
+        vec2d sum {};
+        sum.reserve(rowdim);
+        for(size_t i = 0; i < rowdim; ++i) {
+            std::vector<R> temp{};
+            temp.reserve(coldim);
+
+            std::transform(begin(left.data_[i]),
+                    end(left.data_[i]),
+                    begin(right.data_[i]),
+                    std::back_inserter(temp),
+                    [](R a, R b) {return a+b;});
+
+            sum.emplace_back(std::move(temp));
+        }
+        return sum;
+    }
+
+    friend bool operator==(const Matrix &left, const Matrix &right)
+    {
+        return left.data_ == right.data_;
+    }
+
+
 };
 
 template<typename R>
-inline constexpr Matrix<R>::Matrix(ilist init_list) noexcept
-    : data_(init_list) {}
+inline Matrix<R>::Matrix(ilist init_list)
+    : data_{init_list}, 
+      rowdim_{data_.size()},
+      coldim_{rowdim_ > 0 ? data_[0].size() : 0} 
+{}
+
+template<typename R>
+inline Matrix<R>::Matrix(vec2d vec)
+    : data_{vec}, 
+      rowdim_{data_.size()},
+      coldim_{rowdim_ > 0 ? data_[0].size() : 0} 
+{}
 
 
 } // namespace homology
